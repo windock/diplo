a60f6a68d08d8ca4641a3d3257604f4d28327e31
Andrew Shcheglov <godwindock@gmail.com>
2016-02-21 22:54:40 +0200

    pretty products

--- Gemfile
+++ Gemfile
@@ -24,6 +24,7 @@ group :development do
   gem 'quiet_assets'
   gem 'rails_layout'
   gem 'spring-commands-rspec'
+  gem 'twitter-bootstrap-rails'
 end
 group :development, :test do
   gem 'factory_girl_rails'
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,9 @@
 class Product < ActiveRecord::Base
+  enum skin_type: [ :dry, :normal, :oily, :combination, :sensitive ]
+  enum concern: [ :daily_care, :blemish, :cleanse, :daily_sun_care, :de_aging,
+                  :deodorant, :dullness, :eye_area, :foundation, :hydration,
+                  :moisturize, :pores, :redness, :shave, :sun_protection,
+                  :tone, :unknown ]
+
   validates :name, :title, :concern_id, :skin_type, :sku, presence: true
 end
