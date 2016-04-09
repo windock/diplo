320ae13c7345bfd5b7f3b374898a4cd49beb7ed7
Andrew Shcheglov <godwindock@gmail.com>
2016-04-09 14:59:10 +0300

    Experimental test

--- spec/models/profile_spec.rb
+++ spec/models/profile_spec.rb
@@ -2,4 +2,29 @@ require 'rails_helper'
 
 RSpec.describe Profile, type: :model do
   pending "add some examples to (or delete) #{__FILE__}"
+
+  before do
+    @markets = [Market.create(code: 'm1'), Market.create(code: 'm2'), Market.create(code: 'm3')]
+    @profile = Profile.new(name: 'profile1', markets: @markets.take(1)).save
+  end
+
+  it 'works' do
+    ActiveRecord::Base.transaction do
+      @profile.markets = markets.take(2)
+      expect(@profile.markets).to contain_exactly(@markets[0], @markets[1])
+      raise ActiveRecord::Rollback
+    end
+    expect(@profile.markets).to contain_exactly(@markets[0], @markets[1])
+    @profile.reload
+    expect(@profile.markets).to contain_exactly(have_attributes(code: 'm1'))
+  end
+
+  it 'with attributes' do
+  end
+
+  after do
+    ProfileMarket.destroy_all
+    Market.destroy_all
+    Profile.destroy_all
+  end
 end
